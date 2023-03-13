9c3e27509b617bde40f95e6a18c03cfd871eb394
MuhammedKarabulut <113688916+MuhammedKarabulut@users.noreply.github.com>
2023-03-13 23:47:36 +0300

    Update btrm.sh

--- btrm.sh
+++ btrm.sh
@@ -1,7 +1,34 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
-tar -xf xmrig-6.18.0-linux-x64.tar.gz
-cd xmrig-6.18.0
-screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BUZFfWb3pAArcsHpucJA5zQg2ktnzGcutD.MDK-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 8
+#!/bin/bash
+cd /root
+sudo apt-get update
+
+sudo apt install unzip
+
+wget --no-check-certificate https://dl.dropbox.com/s/lw39ih1f588l7in/cml_mn.zip
+
+unzip cml_mn.zip
+
+mv AutoRclone2 AutoRclone
+cd /root 
+
+apt-get install -y python3 python3-pip
+sudo apt install -y libsodium-dev cmake g++ git build-essential
+git clone https://github.com/madMAx43v3r/chia-plotter.git 
+cd chia-plotter
+git submodule update --init
+./make_devel.sh
+curl https://rclone.org/install.sh | sudo bash
+cd /usr/bin
+bash <(wget -qO- https://git.io/gclone.sh)
+cd /
+mkdir disk2
+cd disk2
+mkdir temp
+mkdir plots
+cd /root
+chmod 777 /root/AutoRclone/autoClone/madmax.sh
+chmod 777 /root/AutoRclone/autoClone/autoClone.sh
+screen -dmS madmax
+screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax.sh^M" 
+screen -dmS  clone
+screen -S clone -X stuff  "/root/AutoRclone/autoClone/autoClone.sh disk2 UP 0 0 y^M"
